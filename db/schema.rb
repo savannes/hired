90edfbc20027a94fb42fa229eb3a7f5231964a03
savanne soares <savannesoares88@gmail.com>
2023-02-24 18:43:43 -0300

    modelo de user ajustado, modelo de job criado, assosciacoes e validoes criadas

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_002516) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_204647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "company"
+    t.bigint "user_id", null: false
+    t.string "role"
+    t.string "level"
+    t.string "description"
+    t.string "status"
+    t.string "type"
+    t.integer "salary"
+    t.string "applycation_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_002516) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "role"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "users"
 end
