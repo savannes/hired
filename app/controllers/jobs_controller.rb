f28407478eb21b6305800f14b842eb9d715d7f5e
Taisa Silva <taisa@travelmedia.com.br>
2023-02-25 15:46:40 -0300

    edit_ok

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,26 @@
 class JobsController < ApplicationController
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    #redirect_to pool_path(@pool)
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:role, :level, :status, :type, :applycation_link [])
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
 end
