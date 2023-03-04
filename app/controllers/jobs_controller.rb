a25f9ddd4e9756f3000b84d31f9e143df9ee5f18
Taisa Silva <taisa@travelmedia.com.br>
2023-03-04 14:26:22 -0300

    pundit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,35 +1,42 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def edit
     @job = Job.find(params[:id])
+    authorize @job
   end
 
   def update
     @job = Job.find(params[:id])
     @job.update(job_params)
+    authorize @job
   end
 
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
+    authorize @job
   end
 
   def index
     @jobs = Job.all
+    @jobs = policy_scope(Job)
   end
 
   def show
     @job = Job.find(params[:id])
+    authorize @job
   end
 
   def new
     @job = Job.new
+    authorize @job
   end
 
   def create
     @job = Job.new(job_params)
     @job.user = current_user
+    authorize @job
     if @job.save!
       redirect_to jobs_path
     else
