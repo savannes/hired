c1876693229e3ec578b8ff03a80858906d085a14
Taisa Silva <taisa@travelmedia.com.br>
2023-03-11 15:18:46 -0300

    filtro

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -6,15 +6,32 @@ export default class extends Controller {
 
   connect() {
   }
-  
+
   search() {
     const query = this.inputTarget.value.toLowerCase();
+    this.resetSearch();
+    const cards = this.cardsTargets;
+    cards.forEach((card) => {
+      if (!card.dataset.keywords.includes(query) && query !== "") {
+        card.classList.add("d-none")
+      }
+    })
+  }
+
+  resetSearch() {
     const cards = this.cardsTargets;
     cards.forEach((card) => {
       card.classList.remove("d-none")
     })
+
+  }
+
+  searchRole(event) {
+    this.resetSearch();
+    const cards = this.cardsTargets;
+    const query = event.target.value;
     cards.forEach((card) => {
-      if (!card.dataset.keywords.includes(query) && query !== "") {
+      if (card.dataset.role !== query) {
         card.classList.add("d-none")
       }
     })
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,9 @@
 class Job < ApplicationRecord
+  ROLE = ["Full Stack", "Front-End", "Back-End"].freeze
   belongs_to :column
   validates :company, :role, :level, :status, :job_type, :application_link, presence: true
   validates :salary, numericality: { only_integer: true }, allow_nil: true
+  validates :role, inclusion: { in: ROLE }
   delegate :user, to: :column
 
   def status
