3a5b321c9940773252246be12d5804a363025fb4
Marina Lameirao <marinalameirao@hotmail.com>
2023-03-14 22:04:32 -0300

    styling

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -5,5 +5,14 @@ class CalendarEventsController < ApplicationController
   end
 
   def create
+  # @calendar_events = CalendarEvent.new(params[:calendar_events])
+  # @calendar_events.save
+  # redirect_to new_calendar_event_path(@calendar_events)
   end
 end
+
+# private
+
+# def calendar_event_params
+#   params.require(:calendar_events).permit(:name, :description, :starts_at, :ends_at)
+# end
