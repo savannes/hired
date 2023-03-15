6518fc30ba0ce3778f0612f355bb928fa852a009
Luiz Campedelli <vapeprosper@gmail.com>
2023-03-14 23:43:39 -0300

    frontendless + sortable in progress

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -11,77 +11,113 @@ export default class extends Controller {
       var droppedTarget = null;
 
       var move = (id, columnId) => {
-      const url = `/move/${id}`;
-      const data = { column_id: columnId };
+        const url = `/move/${id}`;
+        const data = { column_id: columnId };
 
-      fetch(url, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-        },
-        body: JSON.stringify(data),
-      })
+        fetch(url, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+          },
+          body: JSON.stringify(data),
+        });
       }
+
+    const findParent = (element, className, onFind) => {
+      let foundParent = false;
+      let currentElement = element;
+
+      while (!foundParent) {
+        try {
+          if (currentElement.className.includes(className)) {
+            return onFind(currentElement);
+          } else {
+            currentElement = currentElement.parentElement;
+          }
+        } catch (error) {
+          console.warn("Couldn't find parent");
+          break;
+        }
+      }
+    };
+
+    const moveElement = (targetList, position, targetElement) => {
+      // console.log("Target list =>", targetList);
+      // console.log("Position =>", position);
+      // console.log("Target element =>", targetElement);
+
+      droppedTarget = targetList;
+      const droppedColumn = document.getElementById(`list-${targetList.id}`);
+      const index = Array.from(droppedColumn.children).indexOf(targetElement);
+
+      // console.log("Index", index);
+
+      const oldList = clickedTarget;
+      const newIndex = position === "top" ? index : index + 1;
+
+      // console.log("New index =>", newIndex);
+
+      droppedColumn.insertBefore(clickedTarget, droppedColumn.children[newIndex]);
+      move(clickedTarget.id, targetList.id)
+
+      clickedTarget = null;
+    }
     
       items.forEach((item) => {
         item.addEventListener("mousedown", (event) => {
-        event.preventDefault();
-        const clickedItem = event.target.parentElement.parentElement;
-
-        if (clickedItem.tagName == "A" && !clickedTarget) {
-          droppedTarget = null;
-          clickedTarget = clickedItem;
-        }
-      });
+          event.preventDefault();
+          const clickedItem = findParent(event.target, "list-item", (currentElement) => currentElement);
+          
+          if (clickedItem && !clickedTarget) {
+            droppedTarget = null;
+            clickedTarget = clickedItem;
+          }
+        });
       })
 
       document.addEventListener("mouseup", (event) => {
         let targetList = event.target;
         let foundParent = false;
+        let targetElement = null;
+        let position = null;
 
         while (!foundParent) {
           try {
             const tag = targetList.tagName;
+            // console.log("tag", tag);
             if (tag == "LI") {
+              // console.log("entrei");
               const element = event.target.getBoundingClientRect();  
               const cursorPosition = event.clientY - element.top;
               const elementHeight = targetList.clientHeight;
               const middle = elementHeight / 2;
-              console.log("middle", middle);
-              console.log("cursorPosition", cursorPosition);
-              console.log("elementHeight", elementHeight);
+              position = cursorPosition >= middle ? "bottom" : "top";
+              targetElement = targetList;
+            }
 
-              if (cursorPosition >= middle)
-                console.log("bottom");
-              else
-                console.log("top");
+            if (targetList.tagName == "DIV" && targetList.className == "list") {
+              moveElement(targetList, position, targetElement);
+              break;
+            } else {
+              try {
+                targetList = targetList.parentElement;
+              } catch (error) {
+                break;
+              }
             }
           } catch (error) {
             break;
           }
 
-          if (targetList.tagName == "DIV" && targetList.className == "list") {
-            foundParent = true;
-          } else {
-            try {
-              targetList = targetList.parentElement;
-            } catch (error) {
-              break;
-            }
-          }
         }
-
-        if (foundParent && !droppedTarget && clickedTarget) {
-          droppedTarget = targetList;
-          const droppedColumn = document.getElementById(`list-${targetList.id}`);
-          const oldList = clickedTarget.parentElement;
-
-          droppedColumn.appendChild(clickedTarget);
-          move(clickedTarget.id, targetList.id)
+        // console.log("foundParent", foundParent);
+        // // console.log("droppedTaget", droppedTaget);
+        // console.log("clickedTarget", clickedTarget);
+        // if (foundParent && !droppedTarget && clickedTarget) {
+        //   // console.log("targetList", targetList);
           
-          clickedTarget = null;
-        }
+        // }
       });
     }
   }
